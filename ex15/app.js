8c1a1c2b8ecd938e3eeef5ef0a8e1d1b8e201049
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-02-17 20:15:36 +0500

    Questions 16 - 18. Fixed punctuations in question 15.

--- ex15/app.js
+++ ex15/app.js
@@ -1,11 +1,10 @@
 "use strict";
 let guests = ["Abu Hanifa", "Malik", "As Shafi", "Ahmed ibn Hanbal"];
 for (let guest of guests) {
-    console.log("Dear %s, please come over to my house for dinner tonight", guest);
+    console.log("Dear %s, please come over to my house for dinner tonight.", guest);
 }
-console.log("\nGuest %s cannot make it.", guests[3]);
+console.log("\nGuest %s cannot make it.\n", guests[3]);
 guests[3] = "Sufyan At Thawri";
-console.log("\nAfter modifying the guest list:\n");
 for (let guest of guests) {
-    console.log("Dear %s, please come over to my house for dinner tonight", guest);
+    console.log("Dear %s, please come over to my house for dinner tonight.", guest);
 }
---
+++ ex16/app.js
@@ -0,0 +1,18 @@
+"use strict";
+let guests = ["Abu Hanifa", "Malik", "As Shafi", "Ahmed ibn Hanbal"];
+for (let guest of guests) {
+    console.log("Dear %s, please come over to my house for dinner tonight", guest);
+}
+console.log("\nGuest %s cannot make it.", guests[3]);
+guests[3] = "Sufyan At Thawri";
+console.log("\nAfter modifying the guest list:\n");
+for (let guest of guests) {
+    console.log("Dear %s, please come over to my house for dinner tonight", guest);
+}
+console.log("\nI found a bigger table!\n");
+guests.unshift("Al Nafay");
+guests.splice(2, 0, "Hammad");
+guests.push("Al Bukhari");
+for (let guest of guests) {
+    console.log("Dear %s, please come over to my house for dinner tonight", guest);
+}
