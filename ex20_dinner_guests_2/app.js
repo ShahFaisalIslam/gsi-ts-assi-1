06d0ffc44463e878dd28244c31903014a9a49d10
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:24 +0500

    renamed to ex20_dinner_guests_2 for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex20_dinner_guests_2/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let list = [];
+list.push("Everest");
+list.push("Arabic");
+list.push("Mars");
+list.push("Euphrates");
+list.push("Amazon");
---
+++ ex20_dinner_guests_2/app.ts
@@ -0,0 +1,6 @@
+let list : string[] = [];
+list.push("Everest");
+list.push("Arabic");
+list.push("Mars");
+list.push("Euphrates");
+list.push("Amazon");
