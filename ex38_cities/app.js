8a44919cacb7d74f7136cafeb5989c18042803ae
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:25 +0500

    renamed to ex38_cities for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex38_cities/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function describe_city(city = "Karachi", country = "Pakistan") {
+    console.log("%s lies in %s.", city, country);
+}
+describe_city();
+describe_city("Islamabad");
+describe_city("Tokyo", "Japan");
