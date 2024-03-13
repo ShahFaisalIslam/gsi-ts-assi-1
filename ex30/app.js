f907a3e8a7074cdade92c35de4e61d58119c9460
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex30
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex30/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let usernames = ["admin", "root", "viewonly", "nobody", "johndoe"];
-let username;
-for (username of usernames) {
-    if (username == "admin")
-        console.log("Howdy admin! Fancy some wild wild tea?");
-    else
-        console.log("Welcome", username);
-}
--- ex30/app.ts
+++
@@ -1,8 +0,0 @@
-let usernames :string[] = ["admin","root","viewonly","nobody","johndoe"];
-let username : string;
-for (username of usernames) {
-    if (username == "admin")
-        console.log("Howdy admin! Fancy some wild wild tea?");
-    else
-        console.log("Welcome",username);
-}
