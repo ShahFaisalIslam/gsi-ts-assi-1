d2b7aab7d8d869dbe029d61901da4b7b580b6359
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:48 +0500

    Committing removal of folder ex06
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex06/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let unstrippedName = " \n\n\t\nHamid Yousuf\n\t\t\t";
-console.log("Unstripped name:'%s'", unstrippedName);
-let strippedName = unstrippedName.trim();
-console.log("Stripped name: '%s'", strippedName);
