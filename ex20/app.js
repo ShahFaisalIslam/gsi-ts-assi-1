e00d8ec7f03de9852e06f0490216a6e8e0324daa
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex20
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex20/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let list = [];
-list.push("Everest");
-list.push("Arabic");
-list.push("Mars");
-list.push("Euphrates");
-list.push("Amazon");
--- ex20/app.ts
+++
@@ -1,6 +0,0 @@
-let list : string[] = [];
-list.push("Everest");
-list.push("Arabic");
-list.push("Mars");
-list.push("Euphrates");
-list.push("Amazon");
