3d05ed04c9abc1092ec2b860678dcaa96ba8d49a
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:23 +0500

    renamed to ex11_names for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex11_names/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["Basit", "Ahmed", "Talha"];
+let aName;
+for (aName of names) {
+    console.log(aName);
+}
---
+++ ex11_names/app.ts
@@ -0,0 +1,5 @@
+let names : string[] = ["Basit","Ahmed","Talha"];
+let aName : string;
+for (aName of names) {
+    console.log(aName);
+}
