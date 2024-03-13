23b8ba0b469d6a56c30b620a6f771bd4cfd5866f
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex37
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex37/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function make_shirt(size = "L", text = "I love TypeScript") {
-    console.log("Shirt size: %s, text on it: '%s'", size, text);
-}
-make_shirt();
-make_shirt("M");
-make_shirt("S", "Let me grow up");
--- ex37/app.ts
+++
@@ -1,8 +0,0 @@
-function make_shirt(size : string = "L",text : string = "I love TypeScript")
-{
-    console.log("Shirt size: %s, text on it: '%s'",size,text);
-}
-
-make_shirt();
-make_shirt("M");
-make_shirt("S","Let me grow up");
