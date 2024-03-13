a9d259c36e0d1e32b781bfc7707799b0541981fc
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex36
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex36/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function make_shirt(size, text) {
-    console.log("Shirt size: %d, text on it: '%s'", size, text);
-}
-make_shirt(8, "Live it once");
--- ex36/app.ts
+++
@@ -1,6 +0,0 @@
-function make_shirt(size : string,text : string)
-{
-    console.log("Shirt size: %s, text on it: '%s'",size,text);
-}
-
-make_shirt("S","Live it once");
