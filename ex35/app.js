36600f69f6729492ce8bf1c82b8950b0d0fb3874
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex35
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex35/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let animals = ["ant", "lion", "eagle"];
-for (let animal of animals) {
-    console.log(animal);
-}
-for (let animal of animals) {
-    console.log("%s is an amazing animal.", animal);
-}
-console.log("All of these animals have two eyes.");
