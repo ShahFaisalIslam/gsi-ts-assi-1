d58f98b45c944e5c20faaa5b3f5feb2c3cb1da55
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:23 +0500

    renamed to ex17_shrinking_guest_list for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex17_shrinking_guest_list/app.js
@@ -0,0 +1,30 @@
+"use strict";
+let guests = ["Abu Hanifa", "Malik", "As Shafi", "Ahmed ibn Hanbal"];
+let guest;
+for (guest of guests) {
+    console.log("Dear %s, please come over to my house for dinner tonight.", guest);
+}
+console.log("\nGuest %s cannot make it.", guests[3]);
+guests[3] = "Sufyan At Thawri";
+for (guest of guests) {
+    console.log("Dear %s, please come over to my house for dinner tonight.", guest);
+}
+console.log("\nI found a bigger table!\n");
+guests.unshift("Al Nafay");
+guests.splice(2, 0, "Hammad");
+guests.push("Al Bukhari");
+for (guest of guests) {
+    console.log("Dear %s, please come over to my house for dinner tonight.", guest);
+}
+console.log("\nI can invite only two guests for dinner.\n");
+while (guests.length > 2) {
+    let uninvited = guests.pop();
+    console.log("Sorry %s, I cannot invite you for the dinner.", uninvited);
+}
+console.log("");
+for (guest of guests)
+    console.log("Dear %s, you are still invited for dinner.", guest);
+while (guests.length != 0)
+    guests.pop();
+for (guest of guests)
+    console.log(guest);
