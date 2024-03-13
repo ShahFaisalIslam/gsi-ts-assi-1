55e2435a28aafea731a2556abf6962ecf51979cd
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex39
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex39/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function city_country(city, country) {
-    return city + ", " + country;
-}
-console.log(city_country("Karachi", "Pakistan"));
-console.log(city_country("Riyadh", "Saudi Arabia"));
-console.log(city_country("Lisbon", "Portugal"));
--- ex39/app.ts
+++
@@ -1,8 +0,0 @@
-function city_country(city : string,country : string)
-{
-    return city + ", " + country;
-}
-
-console.log(city_country("Karachi","Pakistan"));
-console.log(city_country("Riyadh","Saudi Arabia"));
-console.log(city_country("Lisbon","Portugal"));
