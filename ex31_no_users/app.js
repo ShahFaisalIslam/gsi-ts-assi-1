2c580f57ddef895f5283d66e3d0db4f082fe0108
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:24 +0500

    renamed to ex31_no_users for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex31_no_users/app.js
@@ -0,0 +1,12 @@
+"use strict";
+let usernames = ["admin", "root", "viewonly", "nobody", "johndoe"];
+while (usernames.length > 0) {
+    let username = usernames.shift();
+    if (username == "admin")
+        console.log("Howdy admin! Fancy some wild wild tea?");
+    else
+        console.log("Welcome", username);
+}
+if (usernames.length == 0) {
+    console.log("We need to find some users!");
+}
