126014027e877a1a89fe637531a43ce1ba493ca5
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex14
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex14/app.ts
+++
@@ -1,5 +0,0 @@
-let guests : string[] = ["Abu Hanifa","Malik", "As Shafi", "Ahmed ibn Hanbal"];
-let guest : string;
-for (guest of guests) {
-    console.log("Dear %s, please come over to my house for dinner tonight",guest);
-}
