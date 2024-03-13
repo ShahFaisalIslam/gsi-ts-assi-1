b55dac6603e0d72c006b87d528b58eb35847ee66
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:23 +0500

    renamed to ex10_adding_comments for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex10_adding_comments/ex5_app.ts
@@ -0,0 +1,5 @@
+// I found this hadith reported in the collection of 40 hadiths by Shah Waliullah Dehlawi
+// The program concatenates the variable famous_person and the string containing the quote into message and prints message.
+let famous_person : string ="Allah's Messenger SAWS";
+let message : string = famous_person + " once said, \"A nation's leader is their servant.\"";
+console.log(message);
