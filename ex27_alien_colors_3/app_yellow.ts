016fc25e88b48792e76347a046a0be2e1cb6895b
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:24 +0500

    renamed to ex27_alien_colors_3 for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex27_alien_colors_3/app_yellow.ts
@@ -0,0 +1,7 @@
+let alien_color : string = "yellow";
+if (alien_color == 'green')
+    console.log("Player has earned 5 points!");
+else if (alien_color == 'yellow')
+    console.log("Player has earned 10 points!");
+else
+    console.log("Player has earned 15 points!");
