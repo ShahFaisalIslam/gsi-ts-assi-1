d16fb140c1e7e668e4174ff7a59ab0d8bb8e7293
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex34
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex34/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let pizzas = ["fajita", "bihari tikka", "afghani tikka"];
-for (let pizza of pizzas) {
-    // console.log(pizza);
-    console.log("I like %s pizza.", pizza);
-}
-console.log("Pizza that I like are spicy.\nGive me any size from pan to large, and I'll show you I love pizza.\nI'll eat pizza instead of food for three times a day.\nI love pizza!!!");
