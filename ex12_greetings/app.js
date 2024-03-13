8aa28d7cbb76d26204f3f87efb600f1ac7bb09e2
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:23 +0500

    renamed to ex12_greetings for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex12_greetings/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["Basit", "Ahmed", "Talha"];
+let aName;
+for (aName of names) {
+    console.log("Hello %s! Let's have some tea", aName);
+}
---
+++ ex12_greetings/app.ts
@@ -0,0 +1,5 @@
+let names : string[] = ["Basit","Ahmed","Talha"];
+let aName : string;
+for (aName of names) {
+    console.log("Hello %s! Let's have some tea",aName);
+}
