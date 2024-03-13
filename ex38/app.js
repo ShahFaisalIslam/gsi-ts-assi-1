439beee0fe415535174a45413d86b7b260947ebb
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex38
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex38/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function describe_city(city = "Karachi", country = "Pakistan") {
-    console.log("%s lies in %s.", city, country);
-}
-describe_city();
-describe_city("Islamabad");
-describe_city("Tokyo", "Japan");
--- ex38/app.ts
+++
@@ -1,8 +0,0 @@
-    function describe_city (city : string = "Karachi",country : string = "Pakistan")
-    {
-        console.log("%s lies in %s.",city,country);
-    }
-
-    describe_city();
-    describe_city("Islamabad");
-    describe_city("Tokyo","Japan");
