e835b8937db7dc8951bea18088fd1e8be089c68f
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex12
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex12/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let names = ["Basit", "Ahmed", "Talha"];
-let aName;
-for (aName of names) {
-    console.log("Hello %s! Let's have some tea", aName);
-}
--- ex12/app.ts
+++
@@ -1,5 +0,0 @@
-let names : string[] = ["Basit","Ahmed","Talha"];
-let aName : string;
-for (aName of names) {
-    console.log("Hello %s! Let's have some tea",aName);
-}
