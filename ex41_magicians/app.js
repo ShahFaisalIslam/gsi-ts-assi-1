18c4f3cc6e103f066352be593e2a6a188616fc90
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:25 +0500

    renamed to ex41_magicians for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex41_magicians/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let magicians = ["Crowley", "Evola", "Dion"];
+function show_magicians(magicians_list) {
+    for (let magician of magicians_list) {
+        console.log(magician);
+    }
+}
+show_magicians(magicians);
---
+++ ex41_magicians/app.ts
@@ -0,0 +1,10 @@
+let magicians = ["Crowley","Evola","Dion"];
+
+function show_magicians(magicians_list : string[])
+{
+    for (let magician of magicians_list) {
+        console.log(magician);
+    }
+}
+
+show_magicians(magicians);
