60010edcd7f996fb94d867ceac602c2dd1696532
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:24 +0500

    renamed to ex34_pizzas for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex34_pizzas/app.ts
@@ -0,0 +1,7 @@
+let pizzas : string[] = ["fajita","bihari tikka","afghani tikka"];
+let pizza : string;
+for (pizza of pizzas) {
+    // console.log(pizza);
+    console.log("I like %s pizza.",pizza);
+}
+console.log("Pizza that I like are spicy.\nGive me any size from pan to large, and I'll show you I love pizza.\nI'll eat pizza instead of food for three times a day.\nI love pizza!!!");
