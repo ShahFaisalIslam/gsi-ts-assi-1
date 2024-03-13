7bb9e81c7002f2f2bed66cc2c9aed9a55bd07e53
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:23 +0500

    renamed to ex14_guest_list for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex14_guest_list/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let guests = ["Abu Hanifa", "Malik", "As Shafi", "Ahmed ibn Hanbal"];
+let guest;
+for (guest of guests) {
+    console.log("Dear %s, please come over to my house for dinner tonight", guest);
+}
---
+++ ex14_guest_list/app.ts
@@ -0,0 +1,5 @@
+let guests : string[] = ["Abu Hanifa","Malik", "As Shafi", "Ahmed ibn Hanbal"];
+let guest : string;
+for (guest of guests) {
+    console.log("Dear %s, please come over to my house for dinner tonight",guest);
+}
