0936cbbba2052b2c0c5b979f01c5e1c938d209b9
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:49 +0500

    Committing removal of folder ex10
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex10/ex5_app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// I found this hadith reported in the collection of 40 hadiths by Shah Waliullah Dehlawi
-// The program concatenates the variable famous_person and the string containing the quote into message and prints message.
-let famous_person = "Allah's Messenger SAWS";
-let message = famous_person + " once said, \"A nation's leader is their servant.\"";
-console.log(message);
--- ex10/ex8_app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Shah Faisal, 2024-02-17
-// Duplicated in order to keep the amount of questions to 45.
-console.log(4 + 4);
-console.log(12 - 4);
-console.log(2 * 4);
-console.log(16 / 2);
