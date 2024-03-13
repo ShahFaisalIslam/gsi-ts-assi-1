8cbcabe10423455c842a54b96d5163f8505810fb
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:12:23 +0500

    renamed to ex13_your_own_array for better readability
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

---
+++ ex13_your_own_array/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let modes = ["Honda Civic", "Buggati", "Lamborghini", "Tesla"];
+let mode;
+for (mode of modes) {
+    console.log("I would like to own a %s car.", mode);
+}
---
+++ ex13_your_own_array/app.ts
@@ -0,0 +1,5 @@
+let modes : string[] = ["Honda Civic","Buggati","Lamborghini","Tesla"];
+let mode : string;
+for (mode of modes) {
+    console.log("I would like to own a %s car.",mode);
+}
