bfe2c7ccb52ec507ff708df4c87e6a1f985593eb
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-03-13 20:13:48 +0500

    Committing removal of folder ex05
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex05/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let famous_person = "Allah's Messenger SAWS";
-let message = famous_person + " once said, \"A nation's leader is their servant.\"";
-console.log(message);
--- ex05/app.ts
+++
@@ -1,3 +0,0 @@
-let famous_person : string ="Allah's Messenger SAWS";
-let message :string = famous_person + " once said, \"A nation's leader is their servant.\"";
-console.log(message);
