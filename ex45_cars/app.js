e672689fc238033c833b7ea5f449c6b61e2396c5
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-08-03 18:42:19 +0500

    Fixed cars question
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex45_cars/app.js
+++ ex45_cars/app.js
@@ -1,23 +1,11 @@
 "use strict";
-function mfg_car(manufacturer, model, ...args) {
-    let car = {
+;
+function manufactureCar(manufacturer, model, ...features) {
+    let newCar = {
         manufacturer: manufacturer,
-        model: model
+        model: model,
     };
-    for (let i = 0; i < args.length; i++) {
-        switch (i) {
-            case 0:
-                car.color = args[i];
-                break;
-            case 1:
-                car.windows = args[i];
-        }
-    }
-    return car;
+    features.forEach(([key, value]) => newCar[key] = value);
+    return newCar;
 }
-let car_1 = mfg_car("Toyota", "Corolla");
-let car_2 = mfg_car("Honda", "Civic", "Purple");
-let car_3 = mfg_car("Bugatti", "Buggatati", "Black", 7);
-console.log(car_1);
-console.log(car_2);
-console.log(car_3);
+console.log(manufactureCar("Tesla", "CyberTruck", ["color", "Pink"], ["hasTurbo", false]));
--- ex45_cars/app.ts
+++ ex45_cars/app.ts
@@ -1,24 +1,18 @@
-function mfg_car(manufacturer : string, model : string,...args : any[])
-{
-        let car : {manufacturer : string, model : string, color? : string, windows? : number} = {
-            manufacturer : manufacturer,
-            model : model
-        };
-        for (let i = 0;i < args.length;i++) {
-            switch (i) {
-                case 0:
-                    car.color = args[i];
-                    break;
-                case 1:
-                    car.windows = args[i];
-            }
-        }
-        return car;
+interface Car {
+    manufacturer : string,
+    model : string,
+    [optionalFeatures: string] : any
+};
+
+function manufactureCar(manufacturer : string, model: string, ...features: [string,any][]) : Car {
+    let newCar : Car = {
+        manufacturer: manufacturer,
+        model: model,
+            
+    };
+
+    features.forEach(([key,value])=>newCar[key] = value)
+    return newCar;
 }
 
-let car_1 = mfg_car("Toyota", "Corolla");
-let car_2 = mfg_car ("Honda","Civic","Purple");
-let car_3 = mfg_car("Bugatti","Buggatati","Black",7);
-console.log(car_1);
-console.log(car_2);
-console.log(car_3);
+console.log(manufactureCar("Tesla","CyberTruck",["color","Pink"],["hasTurbo",false]));
