b6b47e2748097dfbb088923ba4ef4b855f8e743d
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-08-03 17:57:23 +0500

    Spiced up exercise 13
    Signed-off-by: Shah Faisal <faisal.islam.ceme@gmail.com>

--- ex13_your_own_array/app.js
+++ ex13_your_own_array/app.js
@@ -1,6 +1,9 @@
 "use strict";
 let modes = ["Honda Civic", "Buggati", "Lamborghini", "Tesla"];
 let mode;
+let i = 0;
 for (mode of modes) {
-    console.log("I would like to own a %s car.", mode);
+    let printMode = mode;
+    setTimeout(() => console.log("I would like to own a %s car.", printMode), 1000 * i);
+    i++;
 }
--- ex13_your_own_array/app.ts
+++ ex13_your_own_array/app.ts
@@ -1,5 +1,10 @@
 let modes : string[] = ["Honda Civic","Buggati","Lamborghini","Tesla"];
 let mode : string;
+let i = 0;
 for (mode of modes) {
-    console.log("I would like to own a %s car.",mode);
+    let printMode : string = mode;
+    setTimeout(() =>
+    console.log("I would like to own a %s car.",printMode),
+    1000 * i);
+    i++;
 }
